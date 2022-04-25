35915ded70d81367ab470246e6bb2477b889eaa0
Brijesh Sood <brijesh.kumar@telus.com>
2022-04-25 15:04:42 -0400

    initial commit

---
+++ src/main/java/com/fsl/App.java
@@ -0,0 +1,13 @@
+package com.fsl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
